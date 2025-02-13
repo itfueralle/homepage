55249e0ab8c569469f1dd314084923f8ef2c9631
IT für Alle <kontakt@itfueralle.net>
2025-02-13 21:51:48 +0100

    Added MemberCard, Member-Page and Removed Special Assets Handling

--- src/assets.ts
+++
@@ -1,8 +0,0 @@
-const assets = import.meta.glob<{ default: ImageMetadata }>("/src/assets/**");
-
-export function getAsset(pPath: string) {
-  const path = "/src/assets/" + pPath;
-
-  if (!assets[path]) throw new Error(`${path} does not exist in assets`);
-  return assets[path]();
-}
