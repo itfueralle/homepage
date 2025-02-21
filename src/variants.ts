62fa264788c905744210c8940e7bd9b105e0dc4d
IT für Alle <kontakt@itfueralle.net>
2025-02-21 16:46:28 +0100

    Refactored Variants

--- src/variants.ts
+++ src/variants.ts
@@ -1,21 +1,9 @@
-export class Variant {
-  private id: string;
-
-  constructor(pId: string) {
-    this.id = pId;
-  }
-
-  public getId(): string {
-    return this.id;
-  }
-}
-
-export const variants: Variant[] = [
-  new Variant("taufkirchen"),
-  new Variant("moosburg")
+export const variants = [
+  "taufkirchen",
+  "moosburg"
 ];
 
 export function getStaticPaths() {
-  return variants.map(pVariant => ({params: {variant: pVariant.getId()}, props: {variant: pVariant}}));
+  return variants.map(pVariant => ({params: {variant: pVariant}, props: {variant: pVariant}}));
 }
 
